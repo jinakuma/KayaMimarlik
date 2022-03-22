db693e80c48a558b951b9d2b6ede7ff0c7bd61f8
jinakuma <utlumurat@gmail.com>
2022-03-22 12:18:52 +0300

    FirstInitial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'KayaMimarlik';
+  images = [
+    {
+      imageSrc: 'https://source.unsplash.com/m8Mz6UjaoEo',
+      imageAlt: 'img1',
+    },
+    {
+      imageSrc: 'https://source.unsplash.com/FkxmAuYD8ls',
+      imageAlt: 'img2',
+    },
+    {
+      imageSrc: 'https://source.unsplash.com/7xed5PqcbIQ',
+      imageAlt: 'img3',
+    },
+  ];
 }
---
+++ src/app/carousel/carousel.component.ts
@@ -0,0 +1,67 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+interface carouselImage {
+  imageSrc: string;
+  imageAlt: string;
+}
+
+@Component({
+  selector: 'app-carousel',
+  templateUrl: './carousel.component.html',
+  styleUrls: ['./carousel.component.css'],
+})
+export class CarouselComponent implements OnInit {
+  constructor() {}
+
+  @Input() images: carouselImage[] = [];
+  @Input() indicators = true;
+  @Input() controls = true;
+  @Input() autoSlide = false;
+  @Input() slideInterval = 3000;
+  selectedIndex = 0;
+
+  ngOnInit(): void {
+    if (this.autoSlide) {
+      this.autoSlideImages;
+    }
+  }
+
+  autoSlideImages = setInterval(() => {
+    if (this.selectedIndex === this.images.length - 1) {
+      this.selectedIndex = 0;
+    } else {
+      this.selectedIndex++;
+    }
+  }, this.slideInterval);
+
+  selectedImage(index: number) {
+    this.selectedIndex = index;
+  }
+
+  onPrevClick() {
+    if (this.selectedIndex === 0) {
+      this.selectedIndex = this.images.length - 1;
+    } else {
+      this.selectedIndex--;
+    }
+    clearInterval(this.autoSlideImages);
+    this.autoSlideImages = setInterval(() => {
+      this.checkMaxIndex();
+    }, this.slideInterval);
+  }
+
+  onNextClick() {
+    this.checkMaxIndex();
+    clearInterval(this.autoSlideImages);
+    this.autoSlideImages = setInterval(() => {
+      this.checkMaxIndex();
+    }, this.slideInterval);
+  }
+  checkMaxIndex() {
+    if (this.selectedIndex === this.images.length - 1) {
+      this.selectedIndex = 0;
+    } else {
+      this.selectedIndex++;
+    }
+  }
+}
